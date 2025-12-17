e270db9f98849226e369de44ef7c6833dbf6ec9e
dillibabu1415 <dillibabu1415@gmail.com>
2025-12-17 21:09:09 +0530

    Modify EKS module version and addon configurations
    Updated EKS module version and added conflict resolution fields for cluster addons.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,19 +1,26 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.0"
+  version = "~> 21.0"
 
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
+  # Use the new conflict fields
   cluster_addons = {
     coredns = {
-      most_recent = true
+      most_recent                = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "OVERWRITE"
     }
     kube-proxy = {
-      most_recent = true
+      most_recent                = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "OVERWRITE"
     }
     vpc-cni = {
-      most_recent = true
+      most_recent                = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "OVERWRITE"
     }
   }
 
@@ -21,11 +28,9 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
     instance_types = ["t2.micro"]
-
     attach_cluster_primary_security_group = true
   }
 
@@ -34,13 +39,9 @@ module "eks" {
       min_size     = 1
       max_size     = 2
       desired_size = 1
-
       instance_types = ["t2.micro"]
       capacity_type  = "ON_DEMAND"
-
-      tags = {
-        ExtraTag = "helloworld"
-      }
+      tags = { ExtraTag = "helloworld" }
     }
   }
 
