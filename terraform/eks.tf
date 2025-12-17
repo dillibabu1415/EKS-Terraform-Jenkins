5b96e00284fade60b1c1fe02e4f449cc66dfc1e1
dillibabu1415 <dillibabu1415@gmail.com>
2025-12-17 21:28:22 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,49 +1,60 @@
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  cluster_name                   = local.name
-  cluster_endpoint_public_access = true
+  # Renamed in v20+
+  name                   = local.name
+  endpoint_public_access = true
 
-  # Use the new conflict fields
-  cluster_addons = {
+  # Add-ons: use new conflict arguments
+  addons = {
     coredns = {
-      most_recent                = true
+      most_recent                 = true
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update = "OVERWRITE"
     }
     kube-proxy = {
-      most_recent                = true
+      most_recent                 = true
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update = "OVERWRITE"
     }
     vpc-cni = {
-      most_recent                = true
+      most_recent                 = true
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update = "OVERWRITE"
     }
   }
 
+  # Networking
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["t2.micro"]
-    attach_cluster_primary_security_group = true
-  }
-
+  # Managed node groups (defaults applied per group)
   eks_managed_node_groups = {
     amc-cluster-wg = {
       min_size     = 1
       max_size     = 2
       desired_size = 1
-      instance_types = ["t2.micro"]
-      capacity_type  = "ON_DEMAND"
-      tags = { ExtraTag = "helloworld" }
+
+      # Apply your previous defaults here, per group:
+      ami_type        = "AL2_x86_64"
+      instance_types  = ["t2.micro"]
+
+      # This replaces the old "attach_cluster_primary_security_group = true"
+      # behavior in v21+, which is handled automatically. If you need extra SGs,
+      # use additional security group IDs at the cluster or node level via
+      # module inputs documented in v21.x.
+      capacity_type   = "ON_DEMAND"
+
+      tags = {
+        ExtraTag = "helloworld"
+      }
     }
   }
 
   tags = local.tags
 }
+ 
+
