0fd7667378d36f983e7c4af7c9c4cc931d02fac6
dillibabu1415 <dillibabu1415@gmail.com>
2025-12-17 21:36:51 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,13 +1,28 @@
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  # Renamed in v20+
   name                   = local.name
   endpoint_public_access = true
 
-  # Add-ons: use new conflict arguments
+  # Explicitly set K8s version (you are currently hitting 1.34)
+  kubernetes_version     = "1.34"
+
+  # *** Use AL2023 for 1.34 ***
+  eks_managed_node_groups = {
+    amc-cluster-wg = {
+      min_size      = 1
+      max_size      = 2
+      desired_size  = 1
+
+      ami_type       = "AL2023_x86_64_STANDARD"  # or ARM_64 if Graviton
+      instance_types = ["t2.micro"]
+      capacity_type  = "ON_DEMAND"
+
+      tags = { ExtraTag = "helloworld" }
+    }
+  }
+
   addons = {
     coredns = {
       most_recent                 = true
@@ -26,35 +41,9 @@ module "eks" {
     }
   }
 
-  # Networking
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # Managed node groups (defaults applied per group)
-  eks_managed_node_groups = {
-    amc-cluster-wg = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      # Apply your previous defaults here, per group:
-      ami_type        = "AL2_x86_64"
-      instance_types  = ["t2.micro"]
-
-      # This replaces the old "attach_cluster_primary_security_group = true"
-      # behavior in v21+, which is handled automatically. If you need extra SGs,
-      # use additional security group IDs at the cluster or node level via
-      # module inputs documented in v21.x.
-      capacity_type   = "ON_DEMAND"
-
-      tags = {
-        ExtraTag = "helloworld"
-      }
-    }
-  }
-
   tags = local.tags
 }
- 
-
